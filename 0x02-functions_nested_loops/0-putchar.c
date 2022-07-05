66fec32de91151c4bd7b24481f78615d32a14183
elsateklu7 <elsateklu7@gmail.com>
2022-07-06 00:35:33 +0300

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,21 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c:  print _putchar
  *
  * Return: On success 0.
  * .
  */
-int _putchar(char c)
+int main(void)
 {
-	c ='_putchar';
-	_putchar(c'\n');
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return 0;
 }
